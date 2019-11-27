33217b0617cb7ac23653f87e477443eac30a5b5a
David Winters <davewinters1987@gmail.com>
2019-11-27 23:56:26 +0000

    Made weather component - Brought in weather icons pack

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Weather from './app_component/weather.component';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'weather-icons/css/weather-icons.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
+      <Weather />
     </div>
   );
 }
